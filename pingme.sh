04ce3f5e8b3bd77e3e2e6fa5c125a81443bdea77
Dark Tron <darktron1337@gmail.com>
2021-05-20 15:20:56 +0200

    if connection could not be established no ping will be tried

--- pingme.sh
+++ pingme.sh
@@ -6,37 +6,45 @@ fail=0
 # www1 router2 router3 www2
 for host in 10.7.106.3 10.7.106.4 10.7.106.66 10.7.106.98
 do
-	ssh_cmd="ssh $usr@$host"
+	ssh_cmd="ssh -q $usr@$host"
 
-	echo Connected to $($ssh_cmd hostname)
+	hostna=$($ssh_cmd "hostname")
+	if [ "$?" -eq 0 ];
+	then
+		echo Connected to $hostna
+    	
+		for target in router1 www1 router2 router3 www2
+	    do
+		    # ping with IPv4
+		    $ssh_cmd "ping -4 -c 3 $target > /dev/null"
 
-	for target in router1 www1 router2 router3 www2
-	do
-		# ping with IPv4
-		$ssh_cmd "ping -4 -c 3 $target > /dev/null"
+		    if [ "$?" -eq 0 ];
+		    then
+    			echo IPv4 ping successful to $target
+			else
+				echo IPv4 ping failed to $target
+				fail=1
+			fi
 
-		if [ "$?" -eq 0 ];
-		then
-			echo IPv4 ping successful to $target
-		else
-			echo IPv4 ping failed to $target
-			fail=1
-		fi
+			# ping with IPv6
+			# skip IPv6 ping to localhost
+			[ $target = $($ssh_cmd hostname) ] && continue
 
-		# ping with IPv6
-		# skip IPv6 ping to localhost
-		[ $target = $($ssh_cmd hostname) ] && continue
+			$ssh_cmd "ping -6 -c 3 $target > /dev/null"
 
-		$ssh_cmd "ping -6 -c 3 $target > /dev/null"
+			if [ "$?" -eq 0 ];
+			then
+				echo IPv6 ping successful to $target
+			else
+				echo IPv6 ping failed to $target
+				fail=1
+			fi
+		done
+	
+	else
+		echo Could not connect to $host
+	fi
 
-		if [ "$?" -eq 0 ];
-		then
-			echo IPv6 ping successful to $target
-		else
-			echo IPv6 ping failed to $target
-			fail=1
-		fi
-	done
 	echo ""
 done
 
